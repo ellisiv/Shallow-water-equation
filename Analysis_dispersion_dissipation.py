759a4929a538ca9717c8c702a5da193ce46e7d37
Johanne Skogvang <johanne_skogang@hotmail.com>
2019-03-28 13:55:23 +0100

    oppdatdering av dissipation og dispersion analyse

--- Analysis_dispersion_dissipation.py
+++ Analysis_dispersion_dissipation.py
@@ -9,17 +9,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-#Lax-Friedrich
-
-theta = np.linspace(-np.pi,np.pi,200)
-x_steg=100
-t_steg=10000
+theta = np.linspace(-np.pi,np.pi,100)
+x_steg=900
+t_steg=9000
 dx=1/(x_steg+1)
 dt=1/(t_steg+1)
 beta = (1/dx)*theta
 lambd=np.sqrt(4.905)
-xi=np.sqrt((dt**2-2*lambd*(dt/dx))*np.cos(theta) + (2*lambd/dx)**2)
-#alpha = (1/(beta*dt)*np.arctan((((dt/2)-(2*lambd/dx))*np.cos(beta*dx))/((2*lambd/dx)*np.sin(beta*dx))))
+s = lambd*dt/dx
+
+#Lax_Friedrich
+xi=np.sqrt((1-s**2)*(np.cos(theta))**2 + s**2)
+alpha = (1/(beta*dt)*np.arctan2(s*np.sin(theta),np.cos(theta)))
+plt.figure(1)
 plt.plot(theta,xi)
-#plt.plot(theta, alpha)
+plt.plot(theta, alpha)
+plt.show()
+
+#Mac-Cormack
+xi_MC = np.sqrt((1-s**2 + s**2*np.cos(theta))**2 + (s*np.sin(theta))**2)
+alpha_MC = (1/(beta*dt))*np.arctan2(s*np.sin(theta),(1-s**2+s**2*np.cos(theta)))
+plt.figure(2)
+plt.plot(theta, xi_MC, label='xi')
+plt.plot(theta,alpha_MC, label='alpha')
+plt.legend()
+#plt.ylim(0.9,1.1)
 plt.show()
