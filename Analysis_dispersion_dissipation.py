c2d4347bbe1535059b6593dd207cc45b359190b0
Johanne Skogvang <johanne_skogang@hotmail.com>
2019-03-29 15:59:08 +0100

    disp og diff

--- Analysis_dispersion_dissipation.py
+++ Analysis_dispersion_dissipation.py
@@ -16,22 +16,28 @@ dx=1/(x_steg+1)
 dt=1/(t_steg+1)
 beta = (1/dx)*theta
 lambd=np.sqrt(4.905)
+#lambd=0
 s = lambd*dt/dx
+y = np.ones(len(theta))
 
 #Lax_Friedrich
 xi=np.sqrt((1-s**2)*(np.cos(theta))**2 + s**2)
 alpha = (1/(beta*dt)*np.arctan2(s*np.sin(theta),np.cos(theta)))
 plt.figure(1)
-plt.plot(theta,xi)
-plt.plot(theta, alpha)
+plt.plot(theta,xi, label=r'$\xi$, dispersion' )
+plt.plot(theta, alpha, label =r'$\alpha$, dissipation')
+plt.plot(theta,y, label = 'y = 1', linewidth=0.5)
+plt.legend()
+plt.title('Lax-Friedrich')
 plt.show()
 
 #Mac-Cormack
 xi_MC = np.sqrt((1-s**2 + s**2*np.cos(theta))**2 + (s*np.sin(theta))**2)
 alpha_MC = (1/(beta*dt))*np.arctan2(s*np.sin(theta),(1-s**2+s**2*np.cos(theta)))
 plt.figure(2)
-plt.plot(theta, xi_MC, label='xi')
-plt.plot(theta,alpha_MC, label='alpha')
+plt.plot(theta, xi_MC, label=r'$\xi$, dispersion')
+plt.plot(theta,alpha_MC, label=r'$\alpha$, dissipation')
+plt.plot(theta,y, label = 'y = 1', linewidth=0.5)
 plt.legend()
-#plt.ylim(0.9,1.1)
+plt.title('MacCormack')
 plt.show()
